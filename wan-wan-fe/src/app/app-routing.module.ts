ed4741e428c1d232aee4b82baa1d1f09e7900480
Mario Alexander <marioalexander998@gmail.com>
2019-10-23 22:12:17 +0700

    Re-initiate project repo

---
+++ wan-wan-fe/src/app/app-routing.module.ts
@@ -0,0 +1,28 @@
+import { NgModule } from '@angular/core';
+import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
+
+const routes: Routes = [
+  { path: '', redirectTo: 'home', pathMatch: 'full' },
+  { path: 'home', loadChildren: () => import('./home/home.module').then( m => m.HomePageModule)},
+  { path: 'login', loadChildren: './login/login.module#LoginPageModule' },
+  { path: 'register', loadChildren: './register/register.module#RegisterPageModule' },
+  { path: 'thread-detail', loadChildren: './home/thread-detail/thread-detail.module#ThreadDetailPageModule' },
+  { path: 'thread-category', loadChildren: './home/thread-category/thread-category.module#ThreadCategoryPageModule' },
+  { path: 'add-thread', loadChildren: './add-thread/add-thread.module#AddThreadPageModule' },
+  { path: 'add-thread', loadChildren: './home/add-thread/add-thread.module#AddThreadPageModule' },
+  { path: 'event', loadChildren: './event/event.module#EventPageModule' },
+  { path: 'add-event', loadChildren: './event/add-event/add-event.module#AddEventPageModule' },
+  { path: 'my-event', loadChildren: './event/my-event/my-event.module#MyEventPageModule' },
+  { path: 'event-detail', loadChildren: './event/event-detail/event-detail.module#EventDetailPageModule' },
+  { path: 'profile', loadChildren: './profile/profile.module#ProfilePageModule' },
+  { path: 'edit-profile', loadChildren: './profile/edit-profile/edit-profile.module#EditProfilePageModule' },
+  { path: 'game-list', loadChildren: './profile/edit-profile/game-list/game-list.module#GameListPageModule' },
+];
+
+@NgModule({
+  imports: [
+    RouterModule.forRoot(routes, { preloadingStrategy: PreloadAllModules })
+  ],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
