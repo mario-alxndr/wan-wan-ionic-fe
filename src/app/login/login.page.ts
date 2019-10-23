9fea7f037968374e64cbf68a90ae1ef31e5ca923
Devilslair <39823627+Devilslair@users.noreply.github.com>
2019-10-23 22:34:46 +0700

    routing login&signup, buang yang ga guna, buat tampilan signup
    *login.page.html
    - signup button : route ke signup
    - buang loading dulu, belum kepake
    
    *login.page.ts
    - buang onSubmit ga kepake
    
    *signup
    - back button : route ke login
    - buat tampilan signup(nama, email, password, retype password)

--- src/app/login/login.page.ts
+++ src/app/login/login.page.ts
@@ -39,19 +39,4 @@ export class LoginPage implements OnInit {
   onSwitchAuthMode(){
     this.isLogin = !this.isLogin;
   }
-
-  onSubmit(form: NgForm){
-    if(!form.valid){
-      return;
-    }
-    const email = form.value.email;
-    const password = form.value.password;
-
-    if(this.isLogin){
-
-    }
-    else{
-      
-    }
-  }
 }
