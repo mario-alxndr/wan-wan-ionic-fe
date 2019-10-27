abecaca55758833bffdae8fdf9234bc49fc47fd8
Justin Susanto <39823674+hawkjstn98@users.noreply.github.com>
2019-10-27 11:29:25 +0700

    fix forum routing"

--- wan-wan-fe/src/app/app.module.ts
+++ wan-wan-fe/src/app/app.module.ts
@@ -8,6 +8,7 @@ import { StatusBar } from '@ionic-native/status-bar/ngx';
 
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
+import {EventRoutingModule} from "./event/event-routing.modules";
 
 @NgModule({
   declarations: [AppComponent],
