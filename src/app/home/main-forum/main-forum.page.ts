3beab9bf9ff279df71417ebf2fb5e671e138e05d
Devilslair <39823627+Devilslair@users.noreply.github.com>
2019-11-11 15:46:10 +0700

    forum-logic

--- src/app/home/main-forum/main-forum.page.ts
+++ src/app/home/main-forum/main-forum.page.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { environment } from '../../../environments/environment';
+import axios from 'axios';
 
 @Component({
   selector: 'app-main-forum',
@@ -6,10 +8,26 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./main-forum.page.scss'],
 })
 export class MainForumPage implements OnInit {
-
+  thread_maker = "Thread Maker";
   constructor() { }
-
-  ngOnInit() {
+  
+  ngOnInit() { 
+    var temp_thread_maker;
+    axios.get(environment.endPointConstant.ThreadPageEndPoint)
+    .then(resp => {
+      console.log(resp.data);
+      console.log(resp.data[0].makerUsername);
+      temp_thread_maker = resp.data[0].makerUsername;
+    })
+    .catch(function (error) {
+      console.log(error);
+    });
+    return new Promise(() => {
+      setTimeout(() => {
+        console.log("X" + this.thread_maker);
+        this.thread_maker = temp_thread_maker;
+        console.log("Y" + this.thread_maker);
+      }, 1000);
+    });
   }
-
 }
